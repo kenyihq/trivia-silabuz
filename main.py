e5d1a22d246272f9f474a3a78d90f1c49d7d1ae0
Kenyi Hancco <kenyihq@gmail.com>
2022-09-12 05:16:04 -0500

    Se agregó el reto de la clase 3

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 def ejercicio_1():
     palabra = input("Ingresa una palabra: ")
     numero = int(input("Ingresa el número de veces que se repetirá: "))
@@ -37,6 +38,24 @@ def ejercicio_4():
         print(" ")
         x += 1
 
+def reto_1():
+  x = int(input("Ingrese el primer número: "))
+  y = int(input("Ingrese el segundo número: "))
+  print((x+y)/2)
+
+def reto_2():
+  x = int(input("Ingrese el primer número: "))
+  y = int(input("Ingrese el segundo número: "))
+  print(x**y)
+
+def reto_3():
+  x = int(input("Ingrese el primer número: "))
+  print(x**0.5)
+
+def reto_4():
+  x = int(input("Ingrese el primer número: "))
+  y = int(input("Ingrese el segundo número: "))
+  print((x**2+ y**2)**(1/2))
 
 def run():
     print("B I E N V E N I D O\n")
@@ -47,6 +66,7 @@ def run():
     print("2. Ejercicio 2")
     print("3. Ejercicio 3")
     print("4. Ejercicio 4")
+    print("5. Reto - Invento mis reglas")
     print("\nby Tutankadev\n")
     opcion = input("Ingresa el numero de ejercicio: ")
     while True:
@@ -62,11 +82,35 @@ def run():
         elif opcion == '4':
             ejercicio_4()
             break
+        
+        elif opcion == '5':
+            print("Elegiste RETOS")
+            print("Mi primer programa en Python")
+            print("Clase 2 - Silabuz\n")
+            print("Elije un ejercicio:")
+            print("1. Reto 1")
+            print("2. Reto 2")
+            print("3. Reto 3")
+            print("4. Reto 4")
+            print("\nby Tutankadev\n")
+            reto = input("Ingresa el numero de ejercicio: ")
+            if reto == '1':
+              reto_1()
+              break
+            elif reto == '2':
+              reto_2()
+              break
+            elif reto == '3':
+              reto_3()
+              break
+            elif reto == '4':
+              reto_4()
+              break
+            else:
+              print("Elija una opción válida")
+              run()
+              break
         else:
             print("Elija una opción válida")
             run()
             break
-
-
-if __name__ == '__main__':
-    run()
